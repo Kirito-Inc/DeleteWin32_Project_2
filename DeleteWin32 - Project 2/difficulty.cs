10f3f4e8c8d4caecd8f44820eb3e49df17e71260
Neyylon <owlilfv@gmail.com>
2021-03-18 01:33:16 +0100

    Timer and difficulty
    created timer module, created difficulty class, timer handler.

--- DeleteWin32 - Project 2/difficulty.cs
+++ DeleteWin32 - Project 2/difficulty.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace DeleteWin32___Project_2
 {
-    class difficulty
+    class Difficulty
     {
+        private int startTime;
+        public int StartTime { get { return startTime; } }
+        private int reward;
+        public int Reward { get { return reward; } }
+        private int minSizeX;
+        private int minSizeY;
+        private int maxSizeX;
+        private int maxSizeY;
+        public int MinSizeX { get { return minSizeX; } }
+        public int MinSizeY { get { return minSizeY; } }
+        public int MaxSizeX { get { return maxSizeX; } }
+        public int MaxSizeY { get { return maxSizeY; } }
+        public Difficulty(int startTime, int reward, int minSizeX, int minSizeY, int maxSizeX, int maxSizeY)
+        {
+            this.startTime = startTime;
+            this.reward = reward;
+            this.minSizeX = minSizeX;
+            this.minSizeY = minSizeY;
+            this.maxSizeX = maxSizeX;
+            this.maxSizeY = maxSizeY;
+        }
+        public Difficulty() { }
+
 
     }
 }
